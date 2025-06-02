b6809c4caad006371a70ebf4e0c4d705801e4854
MoonJihoon5788 <munjihun3824@naver.com>
2025-06-02 16:21:55 +0900

    start

---
+++ JihoonLibary_back/src/main/java/com/example/jihoonlibary_back/JihoonLibaryBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jihoonlibary_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihoonLibaryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JihoonLibaryBackApplication.class, args);
+    }
+
+}
---
+++ JihoonLibary_back/src/test/java/com/example/jihoonlibary_back/JihoonLibaryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jihoonlibary_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihoonLibaryBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
